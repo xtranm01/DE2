f09d1d703bccb13b90dfb294343329a3b8a029ce
Tran Minh Hieu (230199) <xtranm01@vutbr.cz>
2022-10-12 12:44:42 +0200

    update

--- labs/03-interrupts/lab3-interrupts_timer/src/main.c
+++ labs/03-interrupts/lab3-interrupts_timer/src/main.c
@@ -14,7 +14,7 @@
 
 /* Defines -----------------------------------------------------------*/
 #define LED_GREEN PB5  // Arduino Uno on-board LED
-#define LED_RED PB0    // External active-low LED
+#define BUTTON PB1    // External active-low LED
 
 
 /* Includes ----------------------------------------------------------*/
@@ -35,11 +35,11 @@ int main(void)
 {
     // Set pins where LEDs are connected as output
     GPIO_mode_output(&DDRB, LED_GREEN);
-    GPIO_mode_output(&DDRB, LED_RED);
+    GPIO_mode_input_pullup(&DDRB, BUTTON);
 
     // Configuration of 16-bit Timer/Counter1 for LED blinking
     // Set the overflow prescaler to 262 ms and enable interrupt
-    TIM2_overflow_16ms();
+    TIM2_overflow_16us();
     TIM2_overflow_interrupt_enable();
 
     // Enables interrupts by setting the global interrupt mask
@@ -64,14 +64,14 @@ int main(void)
  **********************************************************************/
 ISR(TIMER2_OVF_vect)
 {
-  static uint8_t no_of_overflows = 0;
 
-    no_of_overflows++;
-    if (no_of_overflows >= 100)
+    if (GPIO_read(&PINB, BUTTON))
     {
-        // Do this every 6 x 16 ms = 100 ms
-        no_of_overflows = 0;
-        PORTB = PORTB ^ (1<<LED_GREEN);
+        PORTB |= (1<<LED_GREEN);
+    } 
+    else
+    {
+        PORTB &= (0<<LED_GREEN);
     }
     // Else do nothing and exit the ISR
     //PORTB = PORTB ^ (1<<LED_GREEN);
